54f7fff1fcc95a04440d28b7b9b41287daee1790
Sebastian Świerczek <sebastian.swierczek@gmail.com>
2017-07-01 21:20:31 +0200

    Add DiscoverMovieViewModel tests

--- app/src/main/kotlin/com/androidmess/helix/discovery/model/data/DiscoverMovieViewModel.kt
+++ app/src/main/kotlin/com/androidmess/helix/discovery/model/data/DiscoverMovieViewModel.kt
@@ -3,6 +3,7 @@ package com.androidmess.helix.discovery.model.data
 import com.androidmess.helix.common.model.data.Movie
 
 data class DiscoverMovieViewModel(val id: Int, val title: String, val imagePath: String) {
+
     companion object Mapper {
         fun fromMovie(movie: Movie): DiscoverMovieViewModel {
             val imagePath = "https://image.tmdb.org/t/p/w500${movie.posterPath}"
---
+++ app/src/test/kotlin/com/androidmess/helix/discovery/model/data/DiscoverMovieViewModelTest.kt
@@ -0,0 +1,44 @@
+package com.androidmess.helix.discovery.model.data
+
+import com.androidmess.helix.BaseRxTest
+import com.androidmess.helix.common.model.data.Movie
+import com.nhaarman.mockito_kotlin.whenever
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.junit.Test
+import org.mockito.Mock
+
+@Suppress("IllegalIdentifier")
+class DiscoverMovieViewModelTest : BaseRxTest() {
+
+    @Mock
+    lateinit var movie: Movie
+
+    override fun setUp() {
+        super.setUp()
+        whenever(movie.id).thenReturn(1234)
+        whenever(movie.title).thenReturn("test title")
+        whenever(movie.posterPath).thenReturn("/testPath")
+    }
+
+    @Test
+    fun `Should have the same id as model`() {
+        val movieViewModel = DiscoverMovieViewModel.fromMovie(movie)
+
+        assertThat(movieViewModel.id, `is`(movie.id))
+    }
+
+    @Test
+    fun `Should have the same title as model`() {
+        val movieViewModel = DiscoverMovieViewModel.fromMovie(movie)
+
+        assertThat(movieViewModel.title, `is`(movie.title))
+    }
+
+    @Test
+    fun `Should have full image path from model's poster path`() {
+        val movieViewModel = DiscoverMovieViewModel.fromMovie(movie)
+
+        assertThat(movieViewModel.imagePath, `is`("https://image.tmdb.org/t/p/w500${movie.posterPath}"))
+    }
+}
