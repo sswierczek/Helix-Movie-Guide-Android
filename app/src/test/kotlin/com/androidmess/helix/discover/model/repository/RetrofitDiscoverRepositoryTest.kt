c0491290d7fbdd9d2626912ccf61a73249d7990f
sswierczk <sebastian.swierczek@gmail.com>
2018-03-24 15:30:04 +0100

    Fix RetrofitDiscoveryRepository test

--- app/src/test/kotlin/com/androidmess/helix/discover/model/repository/RetrofitDiscoverRepositoryTest.kt
+++ app/src/test/kotlin/com/androidmess/helix/discover/model/repository/RetrofitDiscoverRepositoryTest.kt
@@ -1,10 +1,10 @@
 package com.androidmess.helix.discover.model.repository
 
 import com.androidmess.helix.BaseTest
+import com.androidmess.helix.common.network.NetworkConfig
+import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
-import com.nhaarman.mockito_kotlin.whenever
-import org.junit.Before
 import org.junit.Test
 import retrofit2.Retrofit
 
@@ -15,17 +15,15 @@ class RetrofitDiscoverRepositoryTest : BaseTest() {
         const val TEST_API_KEY = "test_api_key1234"
     }
 
-    val retrofit: Retrofit = mock()
-
-    val retrofitService: RetrofitDiscoverService = mock()
-
-    lateinit var repository: RetrofitDiscoverRepository
-
-    @Before
-    fun setUp() {
-        whenever(retrofit.create(RetrofitDiscoverService::class.java)).thenReturn(retrofitService)
-        repository = RetrofitDiscoverRepository(retrofit, TEST_API_KEY)
+    val retrofitService = mock<RetrofitDiscoverService>()
+    val retrofit = mock<Retrofit> {
+        on { create(RetrofitDiscoverService::class.java) } doReturn retrofitService
     }
+    val networkConfig = mock<NetworkConfig> {
+        on { apiKey } doReturn TEST_API_KEY
+    }
+
+    val repository = RetrofitDiscoverRepository(retrofit, networkConfig)
 
     @Test
     fun `When discover movies request called should use page with given api key`() {
